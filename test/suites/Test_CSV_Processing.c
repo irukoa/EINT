28987d8ad0a3c43d6db06da0228c62565127d8dc
irukoa <irukoa@live.com>
2025-10-23 16:18:40 +0200

    Add unit tests.

--- test/suites/Test_CSV_Processing.c
+++ test/suites/Test_CSV_Processing.c
@@ -36,7 +36,8 @@ void CSV_Processing_Test_check_irregular_file() {
 
   static double tol = 10.0 * DBL_EPSILON;
 
-  FILE *file_stream = fopen("./test/suites/IrregularCSV.dat", "r");
+  FILE *file_stream = NULL;
+  int   status      = 0;
 
   double **CSV       = NULL;
   size_t   csv_ncols = 0;
@@ -44,12 +45,34 @@ void CSV_Processing_Test_check_irregular_file() {
 
   size_t bufsize = 1024;
 
+  file_stream = fopen("./test/suites/IrregularCSV1.dat", "r");
+
   if (file_stream == NULL) {
     fprintf(stderr, "%s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 
-  int status = read_csv(file_stream, bufsize, &CSV, &csv_ncols, &csv_nrows);
+  status = read_csv(file_stream, bufsize, &CSV, &csv_ncols, &csv_nrows);
+  fclose(file_stream);
+
+  assert(status == EXIT_FAILURE);
+  assert(CSV != NULL);
+  assert(csv_ncols == 10);
+  assert(csv_nrows == 9);
+  assert(fabs(CSV[0][0] - (double)1) <= tol);
+  for (size_t i = 0; i < csv_nrows; i++) {
+    free(CSV[i]);
+  }
+  free(CSV);
+
+  file_stream = fopen("./test/suites/IrregularCSV2.dat", "r");
+
+  if (file_stream == NULL) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  status = read_csv(file_stream, bufsize, &CSV, &csv_ncols, &csv_nrows);
   fclose(file_stream);
 
   assert(status == EXIT_FAILURE);
